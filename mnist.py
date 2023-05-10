135716f622b225b73622111502d5a2bb71014a28
kotorinmm <s7766649@gmail.com>
2023-05-10 21:07:13 +0900

    fix

--- mnist.py
+++ mnist.py
@@ -3,7 +3,6 @@ from flask import Flask, request, redirect, render_template, flash
 from werkzeug.utils import secure_filename
 from tensorflow.keras.models import Sequential, load_model
 from tensorflow.keras.preprocessing import image
-from tensorflow.keras.optimizers import Adadelta
 
 
 import numpy as np
@@ -20,7 +19,7 @@ app = Flask(__name__)
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-model = load_model('./model.h5', custom_objects={'Adadelta': Adadelta})
+model = load_model('./model.h5')
 
 
 @app.route('/', methods=['GET', 'POST'])
